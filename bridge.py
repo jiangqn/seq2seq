6a5fdea5e9dc400328127c27c08b9c103ccb2be1
ThomasK427 <1935448858@qq.com>
2019-01-05 23:50:22 +0800

    rerange bridge

--- bridge.py
+++ bridge.py
@@ -11,15 +11,16 @@ class Bridge(nn.Module):
         self._cell_projection = nn.Linear(encoder_output_size, hidden_size)
         self._encoder_output_projection = nn.Linear(encoder_output_size, hidden_size)
 
-    def forward(self, encoder_output, lstm_states):
+    def forward(self, encoder_output, final_encoder_states):
         # encoder_output: Tensor (batch_size, time_step, hidden_size * num_directions)
         # final_encoder_hidden: Tensor (num_layers * num_directions, batch_size, hidden_size)
         # final_encoder_cell: Tensor (num_layers * num_directions, batch_size, hidden_size)
-        final_encoder_hidden, final_encoder_cell = lstm_states
+        final_encoder_hidden, final_encoder_cell = final_encoder_states
         if self._bidirectional:
             final_encoder_hidden = torch.cat(final_encoder_hidden.chunk(chunks=2, dim=0), dim=2)
             final_encoder_cell = torch.cat(final_encoder_cell.chunk(chunks=2, dim=0), dim=2)
         init_decoder_hidden = torch.stack([self._hidden_projection(h) for h in final_encoder_hidden], dim=0)
         init_decoder_cell = torch.stack([self._cell_projection(c) for c in final_encoder_cell], dim=0)
         encoder_memory = self._encoder_output_projection(encoder_output)
-        return encoder_memory, (init_decoder_hidden, init_decoder_cell)
+        init_decoder_states = (init_decoder_hidden, init_decoder_cell)
+        return encoder_memory, init_decoder_states
