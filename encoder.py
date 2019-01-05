26bc6888db447c82c81befff62d1897af43b85db
ThomasK427 <1935448858@qq.com>
2019-01-05 17:06:17 +0800

    update

--- encoder.py
+++ encoder.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 from torch.nn import init
 from torch.nn.rnn import pack_padded_sequence, pad_packed_sequence
-from utils import INIT
+from utils import INIT, reorder_sequence, reorder_lstm_states
 
 class Encoder(nn.Module):
 
@@ -28,15 +28,28 @@ class Encoder(nn.Module):
     def forward(self, src_embedding, src_lens):
         # src_embedding: Tensor (batch_size, time_step, embed_size)
         # src_lens: list (batch_size,)
+        assert src_embedding.size(0) == len(src_lens)
+        # initialize encoder hidden states and cell states
         state_layers = self._init_encoder_hidden.size(0)
         batch_size = len(src_lens)
         hidden_size = self._init_encoder_hidden.size(1)
         size = (state_layers, batch_size, hidden_size)
         init_encoder_hidden = self._init_encoder_hidden.expand(*size)
         init_encoder_cell = self._init_encoder_cell.expand(*size)
-        sort_index = sorted(range(len(src_lens)), key=lambda i: src_lens[i], reverse=True)
         init_encoder_hidden = init_encoder_hidden.contiguous()
         init_encoder_cell = init_encoder_cell.contiguous()
+        # sort the src_embedding and src_lens
+        sort_index = sorted(range(batch_size), key=lambda i: src_lens[i], reverse=True)
+        src_lens = [src_lens[i] for i in sort_index]
+        src_embedding = reorder_sequence(src_embedding, sort_index)
+        # pack the sorted src_embedding and src_lens
         packed_src = pack_padded_sequence(src_embedding, src_lens)
-        packed_output, (final_encoder_hidden, final_encoder_cell) = self._lstm(packed_src, src_lens)
+        packed_output, (final_encoder_hidden, final_encoder_cell) = self._lstm(packed_src, (init_encoder_hidden, init_encoder_cell))
+        # unpack the packed encoder output
         encoder_output, _ = pad_packed_sequence(packed_output)
+        # unsort the encoder output
+        back_map = {index: i for i, index in enumerate(sort_index)}
+        reorder_index = [back_map[i] for i in range(batch_size)]
+        encoder_output = reorder_sequence(encoder_output, reorder_index)
+        (final_encoder_hidden, final_encoder_cell) = reorder_lstm_states((final_encoder_hidden, final_encoder_cell), reorder_index)
+        return encoder_output, (final_encoder_hidden, final_encoder_cell)
--- utils.py
+++ utils.py
@@ -1,1 +1,21 @@
 INIT = 1e-2
+
+def reorder_sequence(sequence_embedding, order):
+    # sequence_embedding: Tensor (batch_size, time_step, embed_size)
+    # order: list (batch_size,)
+    assert sequence_embedding.size(0) == len(order)
+    order = torch.LongTensor(order).cuda()
+    return sequence_embedding.index_select(index=order, dim=0)
+
+def reorder_lstm_states(states, order):
+    # states: (hidden, cell)
+    # hidden: Tensor (num_layers * num_directions, batch_size, hidden_size)
+    # cell: Tensor (num_layers * num_directions, batch_size, hidden_size)
+    assert isinstance(states, tuple)
+    assert len(states) == 2
+    assert states[0].size(1) == len(order)
+    order = torch.LongTensor(order).cuda()
+    return (
+        states[0].index_select(index=order, dim=1),
+        states[1].index_select(index=order, dim=1)
+    )
