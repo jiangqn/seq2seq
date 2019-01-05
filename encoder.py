9e7c8b3b9b8c481d2e6e404b2225e9e999ae4930
ThomasK427 <1935448858@qq.com>
2019-01-05 22:18:17 +0800

    rerange encoder

--- encoder.py
+++ encoder.py
@@ -29,28 +29,35 @@ class Encoder(nn.Module):
     def forward(self, src_embedding, src_lens):
         # src_embedding: Tensor (batch_size, time_step, embed_size)
         # src_lens: list (batch_size,)
-        assert src_embedding.size(0) == len(src_lens)
-        # initialize encoder hidden states and cell states
+        batch_size = src_embedding.size(0)
+        assert batch_size == len(src_lens)
+        init_encoder_hidden, init_encoder_cell = self._get_init_states(batch_size)
+        packed_src, sort_index = self._pack_padded_sequence(src_embedding, src_lens)
+        packed_output, (final_encoder_hidden, final_encoder_cell) = self._lstm(packed_src, (init_encoder_hidden, init_encoder_cell))
+        return self._pad_packed_sequence(packed_output, final_encoder_hidden, final_encoder_cell, sort_index)
+
+    def _get_init_states(self, batch_size):
         state_layers = self._init_encoder_hidden.size(0)
-        batch_size = len(src_lens)
         hidden_size = self._init_encoder_hidden.size(1)
         size = (state_layers, batch_size, hidden_size)
         init_encoder_hidden = self._init_encoder_hidden.expand(*size)
         init_encoder_cell = self._init_encoder_cell.expand(*size)
         init_encoder_hidden = init_encoder_hidden.contiguous()
         init_encoder_cell = init_encoder_cell.contiguous()
-        # sort the src_embedding and src_lens
-        sort_index = sorted(range(batch_size), key=lambda i: src_lens[i], reverse=True)
+        return (init_encoder_hidden, init_encoder_cell)
+
+    def _pack_padded_sequence(self, src_embedding, src_lens):
+        sort_index = sorted(range(len(src_lens)), key=lambda i: src_lens[i], reverse=True)
         src_lens = [src_lens[i] for i in sort_index]
         src_embedding = reorder_sequence(src_embedding, sort_index)
-        # pack the sorted src_embedding and src_lens
         packed_src = pack_padded_sequence(src_embedding, src_lens)
-        packed_output, (final_encoder_hidden, final_encoder_cell) = self._lstm(packed_src, (init_encoder_hidden, init_encoder_cell))
-        # unpack the packed encoder output
+        return packed_src
+
+    def _pad_packed_sequence(self, packed_output, final_encoder_hidden, final_encoder_cell, sort_index):
         encoder_output, _ = pad_packed_sequence(packed_output)
-        # unsort the encoder output
         back_map = {index: i for i, index in enumerate(sort_index)}
-        reorder_index = [back_map[i] for i in range(batch_size)]
+        reorder_index = [back_map[i] for i in range(len(sort_index))]
         encoder_output = reorder_sequence(encoder_output, reorder_index)
-        (final_encoder_hidden, final_encoder_cell) = reorder_lstm_states((final_encoder_hidden, final_encoder_cell), reorder_index)
+        (final_encoder_hidden, final_encoder_cell) = reorder_lstm_states((final_encoder_hidden, final_encoder_cell),
+                                                                         reorder_index)
         return encoder_output, (final_encoder_hidden, final_encoder_cell)
