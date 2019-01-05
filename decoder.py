ba0e67728ca17c4c7632aa7f8dc97f8e33dcf569
ThomasK427 <1935448858@qq.com>
2019-01-06 00:44:30 +0800

    update

--- decoder.py
+++ decoder.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from attention import Attention
 
 class Decoder(nn.Module):
@@ -43,3 +44,50 @@ class Decoder(nn.Module):
 
     def get_init_attn_out(self):
         pass
+
+class MultiLayerLSTMCells(nn.Module):
+
+    def __init__(self, input_size, hidden_size, num_layers, bias=True, dropout=0.0):
+        super(MultiLayerLSTMCells, self).__init__()
+        self._input_size = input_size
+        self._hidden_size = hidden_size
+        self._num_layers = num_layers
+        self._bias = bias
+        self._dropout = dropout
+        self._lstm_cells = nn.ModuleList(
+            nn.LSTMCell(self._input_size, self._hidden_size, self._bias)
+            for _ in range(self._num_layers)
+        )
+
+    def forward(self, input, states):
+        # input: Tensor (batch_size, input_size)
+        # states: (hidden, cell)
+        # hidden: Tensor (num_layers, batch_size, hidden_size)
+        # cell: Tensor (num_layers, batch_size, hidden_size)
+        hidden, cell = states
+        output_hidden = []
+        output_cell = []
+        for i, lstm_cell in enumerate(self._lstm_cells):
+            h, c = lstm_cell(input, (hidden[i], cell[i]))
+            output_hidden.append(h)
+            output_cell.append(c)
+            input = F.dropout(h, p=self._dropout, training=self.training)
+        output_hidden = output_hidden.stack(output_hidden, dim=0)
+        output_cell = output_cell.stack(output_cell, dim=0)
+        return output_hidden, output_cell
+
+    @property
+    def input_size(self):
+        return self._input_size
+
+    @property
+    def hidden_size(self):
+        return self._hidden_size
+
+    @property
+    def num_layers(self):
+        return self._num_layers
+
+    @property
+    def bias(self):
+        return self._bias
