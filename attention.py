cdf451703e21c892d35774b65ca665f76317256d
ThomasK427 <1935448858@qq.com>
2019-01-06 12:52:26 +0800

    fix attention

--- attention.py
+++ attention.py
@@ -9,13 +9,17 @@ class Attention(nn.Module):
         super(Attention, self).__init__()
 
     def forward(self, query, key, value, mask):
+        # query: Tensor (batch_size, hidden_size)
+        # key: Tensor (batch_size, time_step, hidden_size)
+        # value: Tensor (batch_size, time_step, hidden_size)
+        # mask: Tensor (batch_size, time_step)
         score = self._score(query, key)
         probability = self._probability_normalize(score, mask)
         output = self._attention_aggregate(probability, value)
         return output
 
     def _score(self, query, key):
-        return query.matmul(key.transpose(1, 2))
+        return query.unsqueeze(1).matmul(key.transpose(1, 2))
 
     def _probability_normalize(self, score, mask):
         score = score.masked_fill(mask==0, -INF)
@@ -23,4 +27,4 @@ class Attention(nn.Module):
         return probability
 
     def _attention_aggregate(self, probability, value):
-        return probability.matmul(value)
+        return probability.matmul(value).squeeze(1)
