9ad6eb3633a8c04675e8895eea494398843a1ac0
ThomasK427 <1935448858@qq.com>
2019-01-06 13:52:16 +0800

    update

---
+++ model/utils.py
@@ -0,0 +1,39 @@
+import torch
+
+INIT = 1e-2
+INF = 1e18
+
+def reorder_sequence(sequence_embedding, order):
+    # sequence_embedding: Tensor (batch_size, time_step, embed_size)
+    # order: list (batch_size,)
+    assert sequence_embedding.size(0) == len(order)
+    order = torch.LongTensor(order).cuda()
+    return sequence_embedding.index_select(index=order, dim=0)
+
+def reorder_lstm_states(states, order):
+    # states: (hidden, cell)
+    # hidden: Tensor (num_layers * num_directions, batch_size, hidden_size)
+    # cell: Tensor (num_layers * num_directions, batch_size, hidden_size)
+    assert isinstance(states, tuple)
+    assert len(states) == 2
+    assert states[0].size(1) == len(order)
+    order = torch.LongTensor(order).cuda()
+    return (
+        states[0].index_select(index=order, dim=1),
+        states[1].index_select(index=order, dim=1)
+    )
+
+def sequence_mean(sequence, seq_lens, dim):
+    assert sequence.size(0) == len(seq_lens)
+    seq_sum = torch.sum(sequence, dim=dim, keepdim=False)
+    seq_mean = torch.stack([s / l for s, l in zip(seq_sum, seq_lens)], dim=0)
+    return seq_mean
+
+def len_mask(seq_lens):
+    max_len = max(seq_lens)
+    batch_size = len(seq_lens)
+    mask = torch.ByteTensor(batch_size, max_len).cuda()
+    mask.fill_(0)
+    for i, l in enumerate(seq_lens):
+        mask[i, :l].fill_(1)
+    return mask
---
+++ preprocess.py
@@ -0,0 +1,9 @@
+import os
+
+base_path = './data'
+train_src = os.path.join(base_path, 'src-train.txt')
+train_trg = os.path.join(base_path, 'trg-train.txt')
+dev_src = os.path.join(base_path, 'src-dev.txt')
+dev_trg = os.path.join(base_path, 'trg-dev.txt')
+test_src = os.path.join(base_path, 'src-test.txt')
+test_trg = os.path.join(base_path, 'trg-test.txt')
